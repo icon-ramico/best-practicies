d4509bfcb02c8fd9e33d84cf714c8803cce39c56
icon-ramico <97400721+icon-ramico@users.noreply.github.com>
2022-07-18 09:55:09 +0300

    A bunch of best practicies
    Hope this is clear

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import Dynamic from './components/Dynamic';
+import Empty from './components/Empty';
 
 function App() {
+  const msg = () => console.log('Hello');
   return (
-    <></>
+    <>
+      <Empty name='' calculatedValue={6 + 4}>
+        sent children
+      </Empty>
+      <br />
+      <Dynamic />
+      <button onClick={msg}></button>
+      <img src='/img.png' alt='Hello' />
+    </>
   );
 }
 
